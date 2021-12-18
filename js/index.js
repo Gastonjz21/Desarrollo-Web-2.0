0b771585727c30a7f485fbb70a8c884398a9144d
Gastonjz21 <gastonjzabala@gmail.com>
2021-12-18 02:34:06 -0300

    Direccion de proyecto

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function AnimacionLogo(pxPantalla){
+    window.addEventListener('scroll', () => {
+        var scroll = document.documentElement.scrollTop;
+        var logo = document.getElementById('logo');
+        
+        if(scroll > pxPantalla){
+            logo.style.marginTop = `${10}px`, logo.style.width = `${60}px`, logo.style.height = `${60}px`;
+        } else {
+            logo.style.marginTop = `${120}px`, logo.style.width = `${200}px`, logo.style.height = `${200}px`;
+        }
+    })
+}
+
+AnimacionLogo(100);
