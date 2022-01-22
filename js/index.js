ea538d1a86120d16d23da2662b624d77686f5499
Gastonjz21 <gastonjzabala@gmail.com>
2022-01-21 21:46:13 -0300

    Detalles a la navegacion responsive

--- js/index.js
+++ js/index.js
@@ -14,11 +14,16 @@ function AnimacionLogo(pxPantalla){
 AnimacionLogo(100);
 
 const navToggle = document.querySelector(".nav-toggle")
+const navDeg = document.querySelector(".nav-toggle")
 const navMenu = document.querySelector("nav")
 const nosotrosA = document.querySelector(".nos")
 const marcasA = document.querySelector(".mar")
 const productsA = document.querySelector(".pro")
 const contactoA = document.querySelector(".con")
+
+navDeg.addEventListener("click", () => {
+    navDeg.classList.toggle("animacion")
+});
 
 
 
@@ -34,19 +39,24 @@ navToggle.addEventListener("click", () => {
 
 nosotrosA.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    navDeg.classList.toggle("animacion");
 });
 
 marcasA.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    navDeg.classList.toggle("animacion");
 });
 
 productsA.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    navDeg.classList.toggle("animacion");
 });
 
 contactoA.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+    navDeg.classList.toggle("animacion");
 });
+
 
 
 
